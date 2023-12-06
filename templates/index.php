5e09d19e3b2223efde73aa84193c03bb26279873
Kévin DIDIER <kevin.didier@iwios.fr>
2023-12-07 00:50:17 +0100

    First Test

--- templates/index.php
+++ templates/index.php
@@ -1,3 +1,3 @@
 <!-- AssoManager/templates/index.php -->
 <h1>Gestion des licenciés</h1>
-<p>Bienvenue dans la gestion des licenciés de ton application !</p>
+<p><?php echo $this->escape($message); ?></p>
