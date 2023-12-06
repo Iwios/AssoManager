c892480813704e1861040cb1b8523bd6f494d784
Kévin DIDIER <kevin.didier@iwios.fr>
2023-12-07 00:24:13 +0100

    First Test

---
+++ lib/Controller/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OCA\AssoManager\Controller;
+
+use OCP\AppFramework\Controller;
+
+class PageController extends Controller
+{
+    /**
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     */
+    public function index()
+    {
+        // La logique de ta page va ici
+        // Tu peux renvoyer une vue, des données JSON, etc.
+    }
+}
