5b38ecc33a4eeb89de7722a8e9f14fc6edcbd3de
Kévin DIDIER <kevin.didier@iwios.fr>
2023-12-07 00:37:53 +0100

    First Test

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -1,24 +1,20 @@
 <?php
 
-namespace OCA\AssoManager\Controller;
+
+namespace OCA\Workspace\Controller;
 
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\TemplateResponse;
 
-class PageController extends Controller
-{
-    /**
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     */
-    public function index()
-    {
-        $params = ['message' => 'Gestion des licenciés'];
+class PageController extends Controller {
+	public function __construct(
+		private UserService $userService
+	) {
+	}
 
-        // Charge la vue depuis le répertoire 'templates' de ton application
-        $template = new TemplateResponse('assomanager', 'index', $params, 'blank');
-
-        // Renvoie la réponse
-        return $template;
-    }
+	public function index(): TemplateResponse {
+		Util::addScript(Application::APP_ID, 'assomanager');
+	
+		return new TemplateResponse('assomanager', 'index', ['isUserGeneralAdmin' => $this->userService->isUserGeneralAdmin(), 'canAccessApp' => $this->userService->canAccessApp() ]); 
+	}
 }
