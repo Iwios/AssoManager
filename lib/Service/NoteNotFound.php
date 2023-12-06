1d88e9345c8d1941ae11f2bb0380029a282b3733
Kévin DIDIER <kevin.didier@iwios.fr>
2023-12-06 13:34:43 +0100

    Clean Dev Branch

--- lib/Service/NoteNotFound.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Kevin DIDIER <contact@iwios.fr>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace OCA\AssoManager\Service;
-
-class NoteNotFound extends \Exception {
-}
--- templates/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Kevin DIDIER <contact@iwios.fr>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-?>
-<div id="content"></div>
