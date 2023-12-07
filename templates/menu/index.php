5154d0aede59a5aa1d365c2233e0628aee73c77c
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-07 14:42:56 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -1,2 +1,2 @@
-<div id="sample"style="display:flex;justify-content:center;"></span><button style="hover:opacity:0,5;border:none;"><span class="icon-category-dashboard">Tableau de bord</button></div>
+<div id="sample"style="display:flex;justify-content:center;"></span><button style="hover:opacity:0,5;border:none;"><div class="icon-category-dashboard"></div>Tableau de bord</button></div>
 
