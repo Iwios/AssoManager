76235645d6a6572cbf7fc7a42598875617252ccf
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-07 16:14:27 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -5,13 +5,13 @@
 <ul class="menuderoulant">
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Tableau de bord</button></div>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des groupes</button></div>
-        <ul style="width:150px;display:flex;justify-content:center;display:flex;flex-direction:column;align-items:center;" class="sousmenu">
-            <li><a href="#">Liste des groupes</a></li>
-            <li><a href="#">Planning des groupes</a></li>
+        <ul style="display:flex;justify-content:center;display:flex;flex-direction:column;align-items:center;" class="sousmenu">
+            <li style="width:150px;"><a href="#">Liste des groupes</a></li>
+            <li style="width:150px;"><a href="#">Planning des groupes</a></li>
         </ul>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des licenciés</button></div>
-        <ul style="width:150px;display:flex;justify-content:center;" class="sousmenu">
-            <li><a href="#">Liste des licenciés</a></li>
+        <ul style="display:flex;justify-content:center;" class="sousmenu">
+            <li style="width:150px;"><a href="#">Liste des licenciés</a></li>
         </ul>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des inscription</button></div>
 </ul>
