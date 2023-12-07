3dac64cc84e8fbeed2460528821a4bb0e257ff37
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-07 14:35:46 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -1,1 +1,2 @@
-<div id="sample"style=display:flex;justify-content:center;><button style=border:none;>Menu</button></div>
+<div id="sample"style="display:flex;justify-content:center;"><span class="icon-category-dashboard"></span><button style="hover:opacity:0,5;border:none;">Tableau de bord</button></div>
+
