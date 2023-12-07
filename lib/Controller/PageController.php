d15f4e48ab28a04d36218fd0b6be29919170926e
Kévin DIDIER <kevin.didier@iwios.fr>
2023-12-07 09:21:18 +0100

    First Test

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -18,7 +18,13 @@ class PageController extends Controller
         // Charge la vue depuis le répertoire 'templates' de ton application
         $template = new TemplateResponse('assomanager', 'index', $params, 'blank');
 
+        // Récupère le contenu de la vue
+        $content = $template->render();
+
+        // Défini le contenu de la réponse
+        $this->response->setContent($content);
+
         // Renvoie la réponse
-        return $template;
+        return $this->response;
     }
 }
--- templates/index.php
+++ templates/index.php
@@ -1,4 +1,7 @@
 <!-- AssoManager/templates/index.php -->
-<h1>Gestion des licenciés</h1>
-<p><?php echo htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
+<?php $this->extend('layout'); ?>
 
+<?php $this->block('content'); ?>
+<h1>Gestion des licenciés</h1>
+<p><?php echo $this->escape($message); ?></p>
+<?php $this->endblock(); ?>
