6d3f0f5481969a3d212c280f770340dd23d33edc
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-07 15:59:52 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -15,3 +15,20 @@
         </ul>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des inscription</button></div>
 </ul>
+
+<style>
+    .sousmenu {
+        display:flex;
+        justify-content: center;
+        padding-left: 30px;
+
+    }
+
+    .menuderoulant .sousmenu {
+        display: none;
+    }
+
+    li:hover .sousmenu {
+        display: block;
+    }
+</style>
