0a6b54c13232923dac41c452504769627cc9aff7
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-07 14:50:41 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -1,2 +1,5 @@
-<div id="sample"style="display:flex;justify-content:center;"></span><button style="hover:opacity:0,5;border:none;display:flex;flex-direction:row;"><div class="icon-category-dashboard"></div>Tableau de bord</button></div>
+<div id="sample"style="display:flex;justify-content:center;"></span><button style="hover:opacity:0,5;border:none;display:flex;flex-direction:row;"><div style="display:flex;align-items:center;" class="icon-category-dashboard"></div>Tableau de bord</button></div>
+<div id="sample"style="display:flex;justify-content:center;"></span><button style="hover:opacity:0,5;border:none;display:flex;flex-direction:row;"><div class="icon-category-dashboard"></div>Gestion des groupes</button></div>
+<div id="sample"style="display:flex;justify-content:center;"></span><button style="hover:opacity:0,5;border:none;display:flex;flex-direction:row;"><div class="icon-category-dashboard"></div>Gestion des licenciés</button></div>
+<div id="sample"style="display:flex;justify-content:center;"></span><button style="hover:opacity:0,5;border:none;display:flex;flex-direction:row;"><div class="icon-category-dashboard"></div>Gestion des inscription</button></div>
 
