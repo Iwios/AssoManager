4b354e1774118c7f61f1f738e76a9a7569a657d0
Kévin DIDIER <kevin.didier@iwios.fr>
2023-12-07 21:45:00 +0100

    Test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -1,22 +1,34 @@
-<!-- template/menu/index.php -->
-
 <head>
     <link rel="stylesheet" href="/css/style.css" />
 </head>
 
-<ul class="menuderoulant" id="appMenu">
-    <!-- ... Autres éléments du menu ... -->
-
-    <li>
-        <div @click="toggleSubMenu('groupes')">
-            <div class="icon-category-dashboard" style="padding-right:20px;"></div>
-            Gestion des groupes
-        </div>
-        <ul v-show="subMenuVisible['groupes']" class="sousmenu">
+<ul class="menuderoulant">
+    <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Tableau de bord</button></div>
+    <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des groupes</button></div>
+        <ul style="display:flex;justify-content:center;display:flex;flex-direction:column;align-items:center;" class="sousmenu">
             <li style="width:150px;"><a href="#">Liste des groupes</a></li>
             <li style="width:150px;"><a href="#">Planning des groupes</a></li>
         </ul>
-    </li>
-
-    <!-- ... Autres éléments du menu ... -->
+    <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des licenciés</button></div>
+        <ul style="display:flex;justify-content:center;" class="sousmenu">
+            <li style="width:150px;"><a href="#">Liste des licenciés</a></li>
+        </ul>
+    <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des inscription</button></div>
 </ul>
+
+<style>
+    .sousmenu {
+        display:flex;
+        justify-content: center;
+        padding-left: 30px;
+
+    }
+
+    .menuderoulant .sousmenu {
+        display: none;
+    }
+
+    li:hover .sousmenu {
+        display: block;
+    }
+</style>
