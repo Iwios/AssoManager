c0d8480f540edf87ed7169d22ed0164dee7f15b7
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-07 16:12:11 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -5,12 +5,12 @@
 <ul class="menuderoulant">
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Tableau de bord</button></div>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des groupes</button></div>
-        <ul style="display:flex;justify-content:center;display:flex;flex-direction:column;align-items:center;" class="sousmenu">
+        <ul style="width:150px;display:flex;justify-content:center;display:flex;flex-direction:column;align-items:center;" class="sousmenu">
             <li><a href="#">Liste des groupes</a></li>
             <li><a href="#">Planning des groupes</a></li>
         </ul>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des licenciés</button></div>
-        <ul style="display:flex;justify-content:center;" class="sousmenu">
+        <ul style="width:150px;display:flex;justify-content:center;" class="sousmenu">
             <li><a href="#">Liste des licenciés</a></li>
         </ul>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des inscription</button></div>
