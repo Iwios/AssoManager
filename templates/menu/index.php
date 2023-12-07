2b28c17ea59e65d41abcf7ba10dbdc1851cb6987
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-07 16:06:27 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -6,12 +6,12 @@
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Tableau de bord</button></div>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des groupes</button></div>
         <ul class="sousmenu">
-            <li><a href="#" style="display:flex;justify-content:center;">Liste des groupes</a></li>
-            <li><a href="#" style="display:flex;justify-content:center;">Planning des groupes</a></li>
+            <li style="display:flex;justify-content:center;"><a href="#">Liste des groupes</a></li>
+            <li style="display:flex;justify-content:center;"><a href="#">Planning des groupes</a></li>
         </ul>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des licenciés</button></div>
         <ul class="sousmenu">
-            <li><a href="#" style="display:flex;justify-content:center;">Liste des licenciés</a></li>
+            <li style="display:flex;justify-content:center;"><a href="#">Liste des licenciés</a></li>
         </ul>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des inscription</button></div>
 </ul>
