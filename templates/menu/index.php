b708853e5fa5658ceff5e7b48a787627a6b8e8ed
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-07 16:10:22 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -5,7 +5,7 @@
 <ul class="menuderoulant">
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Tableau de bord</button></div>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des groupes</button></div>
-        <ul style="display:flex;justify-content:center;display:flex;flex-direction:column;" class="sousmenu">
+        <ul style="display:flex;justify-content:center;display:flex;flex-direction:column;align-items:center;" class="sousmenu">
             <li><a href="#">Liste des groupes</a></li>
             <li><a href="#">Planning des groupes</a></li>
         </ul>
