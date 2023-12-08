d3c15fefdf6fed70720c287ee9060f0f64818ffc
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 10:50:59 +0100

    test

--- templates/contenu/index.php
+++ templates/contenu/index.php
@@ -1,1 +1,51 @@
 <div id="sample">Sample app text here</div>
+
+<div>
+    <button>Ajouté</button>
+    <template>
+        <div>
+            <NcSelect v-model="active" :options="['search-tab', 'settings-tab', 'share-tab']" />
+            <NcAppSidebar
+                name="cat-picture.jpg"
+                subname="last edited 3 weeks ago"
+                :active.sync="active">
+                <NcAppSidebarTab name="Search" id="search-tab">
+                    <template #icon>
+                        <Magnify :size="20" />
+                    </template>
+                    Search tab content
+                </NcAppSidebarTab>
+                <NcAppSidebarTab name="Settings" id="settings-tab">
+                    <template #icon>
+                        <Cog :size="20" />
+                    </template>
+                    Settings
+                </NcAppSidebarTab>
+                <NcAppSidebarTab name="Sharing" id="share-tab">
+                    <template #icon>
+                        <ShareVariant :size="20" />
+                    </template>
+                    Sharing tab content
+                </NcAppSidebarTab>
+            </NcAppSidebar>
+        </div>
+    </template>
+    <script>
+    import Magnify from 'vue-material-design-icons/Magnify'
+    import Cog from 'vue-material-design-icons/Cog'
+    import ShareVariant from 'vue-material-design-icons/ShareVariant'
+
+    export default {
+        components: {
+            Magnify,
+            Cog,
+            ShareVariant,
+        },
+        data() {
+            return {
+                active: 'search-tab',
+            }
+        },
+    }
+    </script>
+</div>
