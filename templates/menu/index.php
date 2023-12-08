557594af78c1ee84e03b46e71d6a890e86990331
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 14:59:47 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -7,7 +7,7 @@
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des groupes</button></div>
         <ul style="display:flex;justify-content:center;display:flex;flex-direction:column;align-items:center;" class="sousmenu">
             <li style="width:150px;"><a href="<?php echo($_['url']['liste_groupe']); ?>">Liste des groupes</a></li>
-            <li style="width:150px;"><a href="/planning_groupe.php">Planning des groupes</a></li>
+            <li style="width:150px;"><a href="../planning_groupe.php">Planning des groupes</a></li>
         </ul>
     <div id="sample" style="display:flex;justify-content:center;"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des licenciés</button></div>
         <ul style="display:flex;justify-content:center;" class="sousmenu">
