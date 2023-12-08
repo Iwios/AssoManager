af0f7509b1c09cdf8790204817361e757c132b54
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 17:38:46 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -5,19 +5,19 @@
 <ul class="menuderoulant">
     <div id="sample"></span><button href="../index.php" style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Tableau de bord</button></div>
     <div id="sample"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des groupes</button></div>
-        <ul class="sousmenu1">
+        <ul class="sousmenu">
             <li style="width:150px;"><a href="<?php echo($_['url']['liste_groupe']); ?>">Liste des groupes</a></li>
             <li style="width:150px;"><a href="../planning_groupe.php">Planning des groupes</a></li>
         </ul>
     <div id="sample"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des licenciés</button></div>
-        <ul class="sousmenu2">
+        <ul class="sousmenu">
             <li style="width:150px;"><a href="#">Liste des licenciés</a></li>
         </ul>
     <div id="sample"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des inscription</button></div>
 </ul>
 
 <style scoped lang="scss">
-    #sample, .sousmenu1, .sousmenu2 {
+    #sample, .sousmenu {
         display:flex;
         justify-content:center;
     }
@@ -31,7 +31,7 @@
         align-items:center;
     }
 
-    .sousmenu1 {
+    .sousmenu {
         flex-direction:column;
         align-items:center;
         border: solid red 4px;
