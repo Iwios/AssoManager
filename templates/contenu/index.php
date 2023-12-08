8f14a6c072a07c029c3263ff6065341bd1c94b0d
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 15:35:27 +0100

    test

--- templates/contenu/index.php
+++ templates/contenu/index.php
@@ -2,14 +2,12 @@
         <div id="sample">Sample app text here</div>
 
         <div style="display:flex;justify-content:flex-end;">
-            <div>
                 <button>Ajouter</button>
                 <div>
                     <label for="site-search">Licenciés :</label>
                     <input type="search" id="site-search" name="q" />
                     <button>Recherche</button>
                 </div>
-            </div>
         </div>
         <div style="display:flex;justify-content:center;">
             <table style="width:100%;margin: 20px 10px 0px 10px;border:black solid 2px;flex;justify-content:space-between;">
