36691796ff8d2dbc26c6c0990537a5d7aa7694ea
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 12:08:46 +0100

    test

--- templates/index.php
+++ templates/index.php
@@ -7,7 +7,7 @@
 	<div id="app-navigation">
 		<?php print_unescaped($this->inc('menu/index')); ?>
 	</div>
-	<div id="app-content">
+	<div id="app-content" style="width:100%;">
 		<div id="app-content-wrapper" style="width:100%;">
 			<?php print_unescaped($this->inc('contenu/index')); ?>
 		</div>
