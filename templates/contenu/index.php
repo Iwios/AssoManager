8d17fe22c201ae04fbd8be6f3a1fc14421c8cb2d
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 17:54:54 +0100

    test

--- templates/contenu/index.php
+++ templates/contenu/index.php
@@ -13,16 +13,16 @@
             <table style="width:100%;margin: 20px 10px 0px 10px;border:black solid 2px;flex;justify-content:space-between;">
                 <thead>
                     <tr>
-                        <th colspan="1">Nom</th>
-                        <th colspan="1">Prénom</th>
-                        <th colspan="1">Groupe</th>
+                        <th class="tableau" colspan="1">Nom</th>
+                        <th class="tableau" colspan="1">Prénom</th>
+                        <th class="tableau" colspan="1">Groupe</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Justin</td>
-                        <td>Didier</td>
-                        <td>1</td>
+                        <td class="tableau">Justin</td>
+                        <td class="tableau">Didier</td>
+                        <td class="tableau">1</td>
                     </tr>
                 </tbody>
             </table>
@@ -30,11 +30,11 @@
     </div>
 
     <style scoped lang="scss">
-        th, td {
+        .tableau {
             display: flex;
             justify-content: center;
         }
-        
+
         th {
             border:black solid 2px;
         }
