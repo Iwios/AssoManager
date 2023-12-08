edb43306fc434c503567407e66ac154e448964ef
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 11:29:19 +0100

    test

--- templates/contenu/index.php
+++ templates/contenu/index.php
@@ -1,32 +1,33 @@
-<div style="display:flex;flex-direction:column;">
-    <div>
-        <div id="sample">Sample app text here</div>
+<div>
+    <div id="sample">Sample app text here</div>
 
-        <div style="display:flex;justify-content:end;">
-            <button>Ajouté</button>
-            <div>
-                <label for="site-search">Licenciés :</label>
-                <input type="search" id="site-search" name="q" />
-                <button>Recherche</button>
-            </div>
-        </div>
+    <div style="display:flex;justify-content:end;">
+        <button>Ajouté</button>
         <div>
-        <table>
-            <thead>
-                <tr>
-                    <th colspan="1">Nom</th>
-                    <th colspan="1">Prénom</th>
-                    <th colspan="1">Groupe</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Justin</td>
-                    <td>Didier</td>
-                    <td>1</td>
-                </tr>
-            </tbody>
-        </table>
+            <label for="site-search">Licenciés :</label>
+            <input type="search" id="site-search" name="q" />
+            <button>Recherche</button>
         </div>
     </div>
+    <div>
+    <table>
+        <thead>
+            <tr>
+                <th colspan="1">Nom</th>
+                <th colspan="1">Prénom</th>
+                <th colspan="1">Groupe</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Justin</td>
+                <td>Didier</td>
+                <td>1</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
 </div>
+
+
+style="display:flex;flex-direction:column;"
