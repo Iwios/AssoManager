55fe6792178b689668933bbb372684cee58eba73
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 15:21:56 +0100

    test

--- templates/contenu/index.php
+++ templates/contenu/index.php
@@ -9,22 +9,22 @@
                 <button>Recherche</button>
             </div>
         </div>
-        <div>
-        <table style="margin: 0px 10px 0px 10px;border:black solid 2px;flex;justify-content:space-between;">
-            <thead>
-                <tr>
-                    <th colspan="1" style="border:black solid 2px;">Nom</th>
-                    <th colspan="1" style="border:black solid 2px;">Prénom</th>
-                    <th colspan="1" style="border:black solid 2px;">Groupe</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td style=>Justin</td>
-                    <td style=>Didier</td>
-                    <td style=>1</td>
-                </tr>
-            </tbody>
-        </table>
+        <div style="display:flex;justify-content:center;">
+            <table style="margin: 0px 10px 0px 10px;border:black solid 2px;flex;justify-content:space-between;">
+                <thead>
+                    <tr>
+                        <th colspan="1" style="border:black solid 2px;">Nom</th>
+                        <th colspan="1" style="border:black solid 2px;">Prénom</th>
+                        <th colspan="1" style="border:black solid 2px;">Groupe</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style=>Justin</td>
+                        <td style=>Didier</td>
+                        <td style=>1</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
