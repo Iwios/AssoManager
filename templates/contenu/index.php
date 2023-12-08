bed0d241d06b0c10337b27db6a81867bc6666d4c
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-08 13:42:01 +0100

    test

--- templates/contenu/index.php
+++ templates/contenu/index.php
@@ -1,7 +1,30 @@
     <div id="sample">Sample app text here</div>
-    <div>
+    
         
-
+    <button>Ajouté</button>
+    <div>
+                <label for="site-search">Licenciés :</label>
+                <input type="search" id="site-search" name="q" />
+                <button>Recherche</button>
+    </div>
+    
+    <table style="margin: 0px 10px 0px 10px;border:black solid 2px;">
+            <thead style="display:flex;justify-content:space-between;border:black solid 2px;">
+                <tr>
+                    <th colspan="1" style="border:black solid 2px;">Nom</th>
+                    <th colspan="1" style="border:black solid 2px;">Prénom</th>
+                    <th colspan="1" style="border:black solid 2px;">Groupe</th>
+                </tr>
+            </thead>
+            <tbody style="flex;justify-content:space-between;">
+                <tr>
+                    <td style=>Justin</td>
+                    <td style=>Didier</td>
+                    <td style=>1</td>
+                </tr>
+            </tbody>
+        </table>
+        
         <div style="display:flex;justify-content:flex-end;">
             <button>Ajouté</button>
             <div>
@@ -28,4 +51,4 @@
             </tbody>
         </table>
         </div>
-    </div>
+
