7fd0550a6e5005621c89747045ebdfbb0f45ed1b
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-11 16:39:27 +0100

    test

--- appinfo/app.php
+++ appinfo/app.php
@@ -7,5 +7,15 @@
         'order' => 10,
         'href' => $urlGenerator->linkToRoute('assomanager.page.index'),
         'name' => 'AssoManager',
+
+        'id' => 'assomanager_liste_groupe',
+        'order' => 10,
+        'href' => $urlGenerator->linkToRoute('assomanager.page.liste_groupe'),
+        'name' => 'AssoManager',
+
+        'id' => 'assomanager_planning_groupe',
+        'order' => 10,
+        'href' => $urlGenerator->linkToRoute('assomanager.page.planning_groupe'),
+        'name' => 'AssoManager',
     ];
 });
