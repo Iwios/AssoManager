ee6c700696e8b37e924ae057f4195d78ed5a0e57
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-11 14:55:37 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -2,7 +2,7 @@
     <li>
         <input type="checkbox" id="dashboard">
         <label for="dashboard">
-            <div class="icon-category-dashboard"></div><a href="../templates/index.php">Tableau de bord</a>
+            <div class="icon-category-dashboard"></div><a href="../index.php">Tableau de bord</a>
         </label>
     </li>
     <li>
@@ -11,8 +11,8 @@
             <div class="icon-category-dashboard"></div>Gestion des groupes
         </label>
         <ul class="sousmenu">
-            <li><a href="../templates/liste_groupe.php">Liste des groupes</a></li>
-            <li><a href="../templates/planning_groupe.php">Planning des groupes</a></li>
+            <li><a href="../liste_groupe.php">Liste des groupes</a></li>
+            <li><a href="../planning_groupe.php">Planning des groupes</a></li>
         </ul>
     </li>
     <li>
