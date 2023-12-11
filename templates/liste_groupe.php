42d1e8afe34badaaa6c728d65f68676494528626
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-11 15:09:17 +0100

    test

--- templates/liste_groupe.php
+++ templates/liste_groupe.php
@@ -1,8 +1,8 @@
 <div id="app">
     <div id="app-navigation">
-        <?php include_once(__DIR__ . '/menu/index.php'); ?>
+        <?php echo file_get_contents(__DIR__ . '/menu/index.php'); ?>
     </div>
     <div id="app-content">
-        <?php include_once(__DIR__ . '/contenu/liste_groupe.php'); ?>
+        <?php echo file_get_contents(__DIR__ . '/contenu/liste_groupe.php'); ?>
     </div>
 </div>
