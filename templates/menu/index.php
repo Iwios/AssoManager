93aab825a010bf7a3d0e9b9ca070114df92f78b8
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-11 13:55:52 +0100

    test

--- templates/menu/index.php
+++ templates/menu/index.php
@@ -1,43 +1,57 @@
 <ul class="menuderoulant">
-    <div id="sample"></span><button href="../index.php" style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Tableau de bord</button></div>
-    <div id="sample"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des groupes</button></div>
+    <li>
+        <button><div class="icon-category-dashboard"></div>Tableau de bord</button>
+    </li>
+    <li>
+        <button><div class="icon-category-dashboard"></div>Gestion des groupes</button>
         <ul class="sousmenu">
-            <li style="width:150px;"><a href="<?php echo($_['url']['liste_groupe']); ?>">Liste des groupes</a></li>
-            <li style="width:150px;"><a href="../planning_groupe.php">Planning des groupes</a></li>
+            <li><a href="<?php echo($_['url']['liste_groupe']); ?>">Liste des groupes</a></li>
+            <li><a href="../planning_groupe.php">Planning des groupes</a></li>
         </ul>
-    <div id="sample"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des licenciés</button></div>
+    </li>
+    <li>
+        <button><div class="icon-category-dashboard"></div>Gestion des licenciés</button>
         <ul class="sousmenu">
-            <li style="width:150px;"><a href="#">Liste des licenciés</a></li>
+            <li><a href="#">Liste des licenciés</a></li>
         </ul>
-    <div id="sample"></span><button style="background:none;width:220px;border:none;display:flex;flex-direction:row;align-items:center;"><div class="icon-category-dashboard" style="padding-right:20px;"></div>Gestion des inscription</button></div>
+    </li>
+    <li>
+        <button><div class="icon-category-dashboard"></div>Gestion des inscriptions</button>
+    </li>
 </ul>
 
 <style scoped lang="scss">
-    #sample, .sousmenu {
-        display:flex;
-        justify-content:center;
+    .menuderoulant {
+        list-style: none;
+        padding: 0;
     }
 
-    #sample {
-        background:none;
-        width:220px;
-        border:none;
-        display:flex;
-        flex-direction:row;
-        align-items:center;
+    li {
+        display: flex;
+        align-items: center;
+        margin-bottom: 5px;
+    }
+
+    button {
+        background: none;
+        border: none;
+        width: 220px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 5px;
+    }
+
+    .icon-category-dashboard {
+        padding-right: 20px;
     }
 
     .sousmenu {
-        flex-direction:column;
-        align-items:center;
-        border: solid red 4px;
-        display:flex;
-        justify-content: center;
-        padding-left: 30px;
-    }
-
-    .menuderoulant .sousmenu {
         display: none;
+        flex-direction: column;
+        align-items: center;
+        border: solid red 4px;
+        padding-left: 30px;
     }
 
     li:hover .sousmenu {
