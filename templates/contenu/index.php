aa47e91b27f0cb60b54bd62549f30ee3bd9b3ce8
KitsuneHakase <vince_du_63@hotmail.fr>
2023-12-11 17:00:03 +0100

    test

--- templates/contenu/index.php
+++ templates/contenu/index.php
@@ -1,41 +1,72 @@
-    <div>
-        <div id="sample">Sample app text here</div>
+<div>
+    <div id="sample">Sample app text here</div>
 
-        <div style="display:flex;justify-content:flex-end;">
-                <button>Ajouter</button>
-                <div class="recherche">
-                    <label for="site-search">Licenciés :</label>
-                    <input type="search" id="site-search" name="q" />
-                    <button>Recherche</button>
-                </div>
-        </div>
-        <div style="display:flex;justify-content:center;">
-            <table style="width:100%;margin: 20px 10px 0px 10px;border:black solid 2px;flex;justify-content:space-between;">
-                <thead>
-                    <tr>
-                        <th colspan="1">Nom</th>
-                        <th colspan="1">Prénom</th>
-                        <th colspan="1">Groupe</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Justin</td>
-                        <td>Didier</td>
-                        <td>1</td>
-                    </tr>
-                </tbody>
-            </table>
+    <div id="container">
+        <button>Ajouter</button>
+        <div class="recherche">
+            <label for="site-search">Licenciés :</label>
+            <input type="search" id="site-search" name="q" />
+            <button>Recherche</button>
         </div>
     </div>
 
-    <style scoped lang="scss">
-        th, tr, table, td {
-            border: black solid 2px;
-            border-collapse: collapse;
-        }
+    <div style="display:flex;justify-content:center;">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Groupe</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Justin</td>
+                    <td>Didier</td>
+                    <td>1</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 
-        .recherche {
-            display: flex;
-        }
-    </style>
+<style scoped lang="scss">
+
+    #sample {
+        margin: 10px 0;
+    }
+
+    #container {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 20px;
+    }
+
+    button {
+        margin-left: 10px;
+    }
+
+    .recherche {
+        display: flex;
+        margin-top: 10px;
+    }
+
+    label {
+        margin-right: 5px;
+    }
+
+    table {
+        width: 100%;
+        margin: 20px 10px 0 10px;
+        border-collapse: collapse;
+    }
+
+    th, td {
+        border: 2px solid black;
+        padding: 8px;
+        text-align: center;
+    }
+
+
+
+</style>
